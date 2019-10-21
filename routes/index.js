5ad8b83ce8fa3a9edd8a8dd55c2fe55c40011076
sararaj-mobi <saranraj.c@cohisionib.com>
2019-10-22 01:43:24 +0530

    intro summery - 2

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Portfolio | Home' });
 });
 
+// privacy policy
+router.get('/privacy-policy', function(re, res, next) {
+  res.render('privacy-policy', { title: 'Portfolio | Privacy Policy'});
+})
+// terms of service
+router.get('/terms', function(req, res, next) {
+  res.render('terms-service', {title: 'Portfolio | Terms of Service'})
+});
 router.get('/get', function(req, res, next) {
   res.send('respond with a resource');
 });
